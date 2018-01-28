939f7a1949c1c3e3a2f3913e56f174bfa583d11b
Magda <magda@Mac-Magdy.local>
2018-01-28 13:59:38 +0100

    add fixed files

--- designs.js
+++ designs.js
@@ -3,8 +3,41 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+//calls to live
+$(document).ready(function() {
+  let designCanvas=$('#pixel_canvas');
+  let table=$('table');
+  let color=$('input[type="color"]');
 
-}
+//for canvas
+function makeGrid(height, width) {
+   let Height=$('#input_height').val();
+	 let Width=$('#input_width').val();
+	  table.html('');
+    table.css('background-color','white');  //canvas background color in css
+
+
+  // canvas
+
+	for (let h=1; h<=Height ;h++){  //cells up
+    designCanvas.append
+      ('<tr><td></td></tr>');}
+  for (let w=1; w<Width ;w++){   //rows to extend
+$('tr').append
+      ('<td></td>');
+  }}
+
+
+  //buttons
+  $('form').submit(function(e){   //the function make grid with the button
+  e.preventDefault();
+  makeGrid();
+  });
+$('#clear_button').on('click',function(){  //clear all
+$('td').css('background-color','white');
+  });
+$('table').on('click','td',function(evt){   //event listeners for cell colors
+$(evt.target).css({'background-color':color.val()});
+  });
+});
