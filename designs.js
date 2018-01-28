0325dafed7ff19ebf37ad58025799a4d47079aec
Magda <magda@Mac-Magdy.local>
2018-01-28 18:04:19 +0100

    fix errors

--- designs.js
+++ designs.js
@@ -3,41 +3,43 @@
 
 // When size is submitted by the user, call makeGrid()
 
+//Below the code to draw pixel art on a customizable canvas
 
-//calls to live
+//defining variables, accessing the DOM using methods of the document object
 $(document).ready(function() {
   let designCanvas=$('#pixel_canvas');
   let table=$('table');
   let color=$('input[type="color"]');
 
-//for canvas
+//set the size of the cross stitch canvas
 function makeGrid(height, width) {
    let Height=$('#input_height').val();
 	 let Width=$('#input_width').val();
 	  table.html('');
-    table.css('background-color','white');  //canvas background color in css
+    table.css('background-color','white');  
 
 
-  // canvas
-
-	for (let h=1; h<=Height ;h++){  //cells up
+//customizable canvas - height to extend cells, width to extend rows
+	for (let h=1; h<=Height ;h++){
     designCanvas.append
       ('<tr><td></td></tr>');}
-  for (let w=1; w<Width ;w++){   //rows to extend
-$('tr').append
+  for (let w=1; w<Width ;w++){
+      $('tr').append
       ('<td></td>');
   }}
 
-
-  //buttons
-  $('form').submit(function(e){   //the function make grid with the button
+//Buttons
+//Declaring functions and attaching them to DOM objects as event listeners
+//Event listeners is used to trigger grid creation and to modify the grid colors
+//Loops to dynamically clear and create the table based on user input
+$('form').submit(function(e){   //the function make grid with the button
   e.preventDefault();
   makeGrid();
   });
 $('#clear_button').on('click',function(){  //clear all
-$('td').css('background-color','white');
+  $('td').css('background-color','white');
   });
-$('table').on('click','td',function(evt){   //event listeners for cell colors
-$(evt.target).css({'background-color':color.val()});
+$('table').on('click','td',function(evt){
+  $(evt.target).css({'background-color':color.val()});
   });
 });
